b4fcbd3de67b3b9028eb791fca240868a5c4af48
Enrique Reyes <ereyes.kike@gmail.com>
2022-12-19 22:59:56 -0500

    Add Regression_Vehicles_Price

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 from models.my_transformed_target_regressor import MyTransformedTargetRegressor
 from models.my_transformed_target_classifier import MyTransformedTargetClassifier
 from models.boston_model import BostonModel
+from models.vehicle_model import VehicleModel
 from models.iris_model import IrisModel
 
 
@@ -20,6 +21,14 @@ def init():
     global boston_keras_model
     boston_keras_model = MyTransformedTargetRegressor.load(
         'FastAPI_REST/estimators/0101_keras_model')
+
+    global vehicle_sklearn_model
+    vehicle_sklearn_model = pickle.load(
+        open('FastAPI_REST/estimators/0103_sklearn_model.pkl', 'rb'))
+
+    global vehicle_keras_model
+    vehicle_keras_model = MyTransformedTargetRegressor.load(
+        'FastAPI_REST/estimators/0103_keras_model')
 
     global iris_sklearn_model
     iris_sklearn_model = pickle.load(
@@ -48,8 +57,18 @@ def boston(data: BostonModel):
     return {'sklearn_pred': sklearn_pred.item(), 'keras_pred': keras_pred.item()}
 
 
+@app.post('/api/vehicle')
+def vehicle(data: VehicleModel):
+    df = pd.DataFrame(data=data.dict(), index=[0])
+
+    sklearn_pred = vehicle_sklearn_model.predict(X=df)
+    keras_pred = vehicle_keras_model.predict(X=df)
+
+    return {'sklearn_pred': sklearn_pred.item(), 'keras_pred': keras_pred.item()}
+
+
 @app.post('/api/iris')
-def boston(data: IrisModel):
+def iris(data: IrisModel):
     df = pd.DataFrame(data=data.dict(), index=[0])
 
     sklearn_pred = iris_sklearn_model.predict(X=df)
