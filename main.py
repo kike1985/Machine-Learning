180b055c6c6ddf1f856f0e50ee74cc7cd9460ac5
Enrique Reyes <ereyes.kike@gmail.com>
2023-02-13 14:23:57 -0500

    upd

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI
 import uvicorn
 import pickle
+import dill
 import pandas as pd
+import numpy as np
+from progressbar import ProgressBar
 
-from models.my_transformed_target_regressor import MyTransformedTargetRegressor
-from models.my_transformed_target_classifier import MyTransformedTargetClassifier
-from models.boston_model import BostonModel
-from models.vehicle_model import VehicleModel
-from models.iris_model import IrisModel
+from resources.my_transformed_target_regressor import MyTransformedTargetRegressor
+from resources.my_transformed_target_classifier import MyTransformedTargetClassifier
+
+from resources._0101_Regression_Boston.boston_model import BostonModel
+from resources._0103_Regression_Vehicles_Price.vehicle_model import VehicleModel
+
+BostonOutlierTransformer = dill.load(
+    open('resources/_0101_Regression_Boston/outlier_transformer', 'rb'))
+BostonImputeTransformer = dill.load(
+    open('resources/_0101_Regression_Boston/impute_transformer', 'rb'))
+BostonDataTransformer = dill.load(
+    open('resources/_0101_Regression_Boston/data_transformer', 'rb'))
+
+VehiclesOutlierTransformer = dill.load(
+    open('resources/_0103_Regression_Vehicles_Price/outlier_transformer', 'rb'))
+VehiclesImputeTransformer = dill.load(
+    open('resources/_0103_Regression_Vehicles_Price/impute_transformer', 'rb'))
+VehiclesDataTransformer = dill.load(
+    open('resources/_0103_Regression_Vehicles_Price/data_transformer', 'rb'))
 
 
 def init():
@@ -16,27 +33,27 @@ def init():
 
     global boston_sklearn_model
     boston_sklearn_model = pickle.load(
-        open('FastAPI_REST/estimators/0101_sklearn_model.pkl', 'rb'))
+        open('resources/_0101_Regression_Boston/sklearn_model.pkl', 'rb'))
 
     global boston_keras_model
     boston_keras_model = MyTransformedTargetRegressor.load(
-        'FastAPI_REST/estimators/0101_keras_model')
+        'resources/_0101_Regression_Boston/keras_model')
 
     global vehicle_sklearn_model
     vehicle_sklearn_model = pickle.load(
-        open('FastAPI_REST/estimators/0103_sklearn_model.pkl', 'rb'))
+        open('resources/_0103_Regression_Vehicles_Price/sklearn_model.pkl', 'rb'))
 
     global vehicle_keras_model
     vehicle_keras_model = MyTransformedTargetRegressor.load(
-        'FastAPI_REST/estimators/0103_keras_model')
+        'resources/_0103_Regression_Vehicles_Price/keras_model')
 
-    global iris_sklearn_model
-    iris_sklearn_model = pickle.load(
-        open('FastAPI_REST/estimators/0201_sklearn_model.pkl', 'rb'))
+    # global iris_sklearn_model
+    # iris_sklearn_model = pickle.load(
+    #     open('estimators/0201_sklearn_model.pkl', 'rb'))
 
-    global iris_keras_model
-    iris_keras_model = MyTransformedTargetClassifier.load(
-        'FastAPI_REST/estimators/0201_keras_model')
+    # global iris_keras_model
+    # iris_keras_model = MyTransformedTargetClassifier.load(
+    #     'estimators/0201_keras_model')
 
 
 init()
@@ -67,14 +84,14 @@ def vehicle(data: VehicleModel):
     return {'sklearn_pred': sklearn_pred.item(), 'keras_pred': keras_pred.item()}
 
 
-@app.post('/api/iris')
-def iris(data: IrisModel):
-    df = pd.DataFrame(data=data.dict(), index=[0])
+# @app.post('/api/iris')
+# def iris(data: IrisModel):
+#     df = pd.DataFrame(data=data.dict(), index=[0])
 
-    sklearn_pred = iris_sklearn_model.predict(X=df)
-    keras_pred = iris_keras_model.predict(X=df)
+#     sklearn_pred = iris_sklearn_model.predict(X=df)
+#     keras_pred = iris_keras_model.predict(X=df)
 
-    return {'sklearn_pred': sklearn_pred.item(), 'keras_pred': keras_pred.item()}
+#     return {'sklearn_pred': sklearn_pred.item(), 'keras_pred': keras_pred.item()}
 
 
 # -------------------------------------------------------------
