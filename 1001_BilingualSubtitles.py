0f4e92b55b5ea44888b3a38d6fc6964b10480b7a
Enrique Reyes <ereyes.kike@gmail.com>
2023-03-16 10:36:47 -0500

    Update 1001_BilingualSubtitles.py

--- 1001_BilingualSubtitles.py
+++ 1001_BilingualSubtitles.py
@@ -17,54 +17,38 @@ def build_thread(path, progress):
 
     translator = GoogleTranslator(source='en', target='es')
 
-    in_path = f'{base_path}/in/{path}'
-    out_path = f'{base_path}/out/{path}'
-
-    file_in = open(in_path, 'r')
-    text_in = file_in.readlines()
-    file_in.close()
+    with open(f'{base_path}/in/{path}', 'r') as file_in:
+        text_in = file_in.readlines()
 
     text_out, new_lines = [], 0
 
-    bar = progress.add_task(f'[red]thread {threading.get_ident()}...', total=len(text_in))
+    bar = progress.add_task(f'[red]thread {threading.get_ident()} [{len(text_in)} items]', total=len(text_in))
 
     for i, text in enumerate(text_in):
         if bool(re.match(time_pattern, text)) == True and len(text_out) > 1:
             for j in range(3, 10):
                 k = i-j
 
-                time_before = text_in[k]
-
-                if bool(re.match(time_pattern, time_before)) == True:
-                    no_before = text_in[k-1]
-
-                    text_out.insert(i+new_lines-1, no_before)      # Se agrega el numero anterior
-                    new_lines += 1
-                    text_out.insert(i+new_lines-1, time_before)    # Se agrega el tiempo anterior
-                    new_lines += 1
-
-                    # Se agrega el texto anterior que puede ser de varias lineas
+                if bool(re.match(time_pattern, text_in[k])) == True:
                     t_text = ''.join([text_in[k+l] for l in range(1, j-1)])
 
                     try:
-                        t_text = '{\\an8}' + translator.translate(t_text) + '\n\n'
+                        t_text = '<font color="#D900D9">' + translator.translate(t_text) + '</font>\n'
                     except Exception as e:
                         progress.remove_task(bar)
                         return
-
-                    text_out.insert(i+new_lines-1, t_text)
-                    new_lines += 1
-
-                    break
+                    else:
+                        text_out.insert(i+new_lines-2, t_text)
+                        new_lines += 1
+                        break
 
         text_out.append(text)
         progress.update(bar, advance=1)
 
-    progress.remove_task(bar)
+    with open(f'{base_path}/out/{path}', 'w', encoding='utf-8') as file_out:
+        file_out.writelines(text_out)
 
-    file_out = open(out_path, 'w')
-    file_out.writelines(text_out)
-    file_out.close()
+    progress.remove_task(bar)
 
 
 with ThreadPoolExecutor(max_workers=6) as executor:
